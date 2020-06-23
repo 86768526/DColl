2b3e4b4935f7c1e9b93665685c052b17bbcf2222
86768526@qq.com <chenxiangchen@qpower.com>
2020-06-23 09:55:25 +0800

    init commit

---
+++ src/main/java/com/c/dcoll/DcollApplication.java
@@ -0,0 +1,13 @@
+package com.c.dcoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcollApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/c/dcoll/DcollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c.dcoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
